a5dce5a6932eb376abdc329ebbf5faa2291fde2d
Aswini <aswinikumar45@yahoo.com>
2017-03-03 05:06:22 +0000

    Answer page with live data

--- dist/server/list/listdocRouter.js
+++ dist/server/list/listdocRouter.js
@@ -6,5 +6,6 @@ router.post('/add', listCtrl.addList);
 router.get('/', listCtrl.viewList);
 router.post('/invite', listCtrl.inviteFrnds);
 router.post('/addquestion', listCtrl.addquestion);
+router.get('/:id', listCtrl.getQuestion);
 
 module.exports = router;
--- webclient/components/anspage.jsx
+++ webclient/components/anspage.jsx
@@ -4,17 +4,15 @@ import {
     Image,
     Button,
     Divider,
-    Card,
     Icon,
     Breadcrumb,
-    Menu,
     Segment,
     Form,
-    Modal,
-    Popup
+    Modal
 } from 'semantic-ui-react';
 import {PropTypes} from 'react';
 import RichTextEditor from 'react-rte';
+const DisplayAnswer = require('./AnswerDisplay');
 let titlestyle = {
     fontFamily: 'Georgia',
     fontSize: 30,
@@ -27,22 +25,6 @@ let questionstyle = {
     fontWeight: 'serif',
     marginTop: '3%',
     lineHeight: '30px'
-};
-let poststyle1 = {
-    fontFamily: 'Georgia',
-    fontSize: 15,
-    fontWeight: 'serif'
-};
-let ansstyle = {
-    fontFamily: 'Cochin',
-    fontWeight: 'serif'
-};
-let addicon = {
-    marginLeft: '70%'
-};
-let commentstyle = {
-    fontFamily: 'Cochin',
-    fontSize: 18
 };
 let buttonfolstyle = {
     marginLeft: '5%'
@@ -70,10 +52,44 @@ class anspage extends React.Component {
     constructor() {
         super();
         this.state = {
+            active: false,
             value: RichTextEditor.createEmptyValue(),
-            id: ''
+            id: '',
+            objArray: [
+                {
+                    id: 0,
+                    category: '',
+                    tags: '',
+                    heading: '',
+                    question: '',
+                    image: '',
+                    displayImage: '',
+                    profileImage: '',
+                    addedOn: '',
+                    upVotes: '',
+                    downVotes: '',
+                    answerCounts: '',
+                    postedBy: '',
+                    views: 0,
+                    topCards: [
+                        {
+                            createdBy: '',
+                            content: '',
+                            createdOn: '',
+                            image: '',
+                            profileImage: '',
+                            upVote: 0,
+                            downVote: 0,
+                            isAccepted: true
+                        }
+                    ],
+                    status: {
+                        open: true
+                    }
+                }
+            ]
         };
-        // this.getData = this.getData.bind(this);
+        this.getData = this.getData.bind(this);
     }
     static propTypes = {
         onChange: PropTypes.func
@@ -84,35 +100,23 @@ class anspage extends React.Component {
             this.props.onChange(value.toString('html'));
         }
     };
-    // find({'id':id}).then((docs) => {
-    //     res.send(docs);
-    // }, (err) => {
-    //     res.send(err);
-    // });
 
-    //   getData(){
-    //     $.ajax({
-    //          url: "http://localhost:8080/list",
-    //          type: 'GET',
-    //          success: function(data) {
-    //              console.log(JSON.stringify(data, undefined, 2));
-    //              this.setState({objArray: data});
-    //          }.bind(this),
-    //          error: function(err) {
-    //              console.log('error occurred on AJAX');
-    //              console.log(err);
-    //          }.bind(this)
-    //      });
-    //  }
-
-    // componentDidMount() {
-    //     let id = window.location.hash.split('id=')[1];
-    //     // this.setState({id: id});
-    //     // console.log('id:'+id);
-    //     this.getData();
-    // }
+    getData() {
+        let id = window.location.hash.split('id=')[1];
+        $.ajax({
+            url: 'http://localhost:8080/list/' + id,
+            type: 'GET',
+            success: function(data) {
+                // console.log('inside ajax call', JSON.stringify(data, undefined, 2));
+                this.setState({objArray: data});
+            }.bind(this)
+          });
+    }
+    componentWillMount() {
+        this.getData();
+    }
     render() {
-        // let id = window.location.hash.split('id=')[1];
+        let quesObj = this.state.objArray;
         return (
 
             <Grid divided='vertically'>
@@ -120,49 +124,40 @@ class anspage extends React.Component {
                     <Grid.Column width={2}/>
                     <Grid.Column width={10}>
                         <div style={titlestyle}>
-                            Firebase Invites for React Native
+                            {quesObj[0].heading}
                             <texteditor/>
                             <Icon style={followstyle} name='add' size='small' color='green'/>
                         </div>
-                        <div style={questionstyle}>We are currently using Firestack on a React
-                          Native project which is great,
-                           however we also need the features available via Firebase's "Invites"
-                            capability. Is anyone aware of
-                            a React Native module (eg on github) for Firebase Invites,
-                            iOS and Android ?</div>
+                        <div style={questionstyle}>{quesObj[0].question}</div>
                         <Breadcrumb>
-                            <Breadcrumb.Section link>Javascript</Breadcrumb.Section>
-                            <Breadcrumb.Divider icon='right angle'/>
-                            <Breadcrumb.Section link>React</Breadcrumb.Section>
-                            <Breadcrumb.Divider icon='right angle'/>
-                            <Breadcrumb.Section link>Semantic UI</Breadcrumb.Section>
+                            <Breadcrumb.Section link>{quesObj[0].tags}</Breadcrumb.Section>
                         </Breadcrumb>
                         <Segment floated='right'>
-                            <div>Asked on feb 14th</div>
-                            <Image floated='left' size='mini'
-                              src='http://semantic-ui.com/images/avatar/large/steve.jpg'/>
+                            <div>Asked on {quesObj[0].addedOn}</div>
+                            <Image floated='left' size='mini' src={quesObj[0].profileImage}/>
                             <a>
-                                Peter
+                                {quesObj[0].postedBy}
                             </a>
                         </Segment>
                         <div style ={crumstyle}>
-                            <Icon style={likestyle} name='thumbs up' size='large' color='green'/>20
-                            <Icon style={unlikestyle} name='thumbs down' size='large' color='red'/>5
+                            <Icon style={likestyle} name='thumbs up' size='large' color='green'/>
+                            {quesObj[0].upVotes}
+                            <Icon style={unlikestyle} name='thumbs down' size='large' color='red'/>
+                            {quesObj[0].downVotes}
                             <Button basic color='blue' content='Views' style={viewstyle} label={{
                                 as: 'a',
                                 basic: true,
                                 color: 'blue',
                                 pointing: 'left',
-                                content: '1,048'
+                                content: quesObj[0].views
                             }}/>
-                            <Modal trigger={< Button positive style = {
+                            <Modal trigger={<Button positive style = {
                                 buttonfolstyle
                             }
-                            size = 'mini' > Click to Answer < /Button>}>
-                            <Modal.Content>
+                            size = 'mini'> Click to Answer </Button>}>
+                                <Modal.Content>
                                     <Form>
-                                        <RichTextEditor
-                                          value={this.state.value}
+                                        <RichTextEditor value={this.state.value}
                                           onChange={this.onChange}/>
                                       </Form>
                                 </Modal.Content>
@@ -177,71 +172,14 @@ class anspage extends React.Component {
                             </Modal>
                             <Button negative style ={buttonfolstyle} size='mini'>Report</Button>
                         </div>
-                        <div style={ansstyle1}>23 Answers</div>
+                        <div style={ansstyle1}>{quesObj[0].answerCounts}
+                            Answers</div>
                         <Divider clearing/>
-                        <div>
-                          <Card fluid>
-                                <Card.Content
-                                  style={poststyle1}>
-                                    <Image
-                                      floated='left'
-                                      size='mini'
-                                      src='http://semantic-ui.com/images/avatar/large/steve.jpg'/>
-                                    <a>
-                                        Peter
-                                      </a>
-                                    <div>Answered on feb 14th
-                                        <Icon name='add user'
-                                          style={addicon} size='big' color='blue'/></div>
-                                      </Card.Content>
-                                <Card.Content>
-                                  <Card.Header style={ansstyle}>
-                                        Updating parent's variable with parent.variable is a wrong
-                                         thought on OOP. When you extend
-                                        the parent class, think like your child class became
-                                        one with the parent, they are not separated anymore,
-                                        so think like the variable is child's from now on.
-                                        But just Updating parent's variable with parent.variable
-                                        is a wrong thought on OOP. When you extend the parent
-                                         class, think like your child class became one with
-                                        the parent, they are not separated anymore, so think
-                                         like the variable is child's now on.
-                                    </Card.Header>
-                                </Card.Content>
-                                <Menu style={commentstyle}>
-                                    <Menu.Item>
-                                        <Icon name='thumbs up' size='large' color='green'/>
-                                        32
-                                    </Menu.Item>
-                                    <Menu.Item>
-                                        <Icon name='thumbs down' size='large' color='red'/>
-                                        6
-                                    </Menu.Item>
-                                    <Menu.Item>
-                                        <Icon name='checkmark' size='large' color='green'/>
-                                        6
-                                    </Menu.Item>
-                                    <Menu.Item>
-                                        Add comment
-                                    </Menu.Item>
-                                    <Menu.Menu position='right'>
-                                        <Menu.Item>
-                                            <Popup on='click' trigger={<
-                                              Icon name = 'ellipsis horizontal'
-                                               size = 'large' />} hideOnScroll>
-                                                <Menu vertical>
-                                                    <Menu.Item>Comments</Menu.Item>
-                                                    <Menu.Item>Save Answer</Menu.Item>
-                                                </Menu>
-                                            </Popup>
-                                        </Menu.Item>
-                                    </Menu.Menu>
-                                </Menu>
-                            </Card>
-                        </div>
+                        <DisplayAnswer ansCollection={this.state.objArray}/>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
+
         );
     }
 }
