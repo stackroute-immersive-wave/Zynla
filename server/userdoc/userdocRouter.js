a0e174ed4e46965328225fd2e3fdff1dff51683d
Soundar <soundar.ksrcts@gmail.com>
2017-03-08 10:50:02 +0000

    userdoc controller added

--- server/userdoc/userdocRouter.js
+++ server/userdoc/userdocRouter.js
@@ -1,136 +1,27 @@
 'use strict';
-const logger = require('./../../applogger');
+// const logger = require('./../../applogger');
 const router = require('express').Router();
-const UserModel = require('../users/userProfileEntity').userModel;
-
-router.post('/add', function(req, res) {
-   logger.debug('Inside user post');
-   let newUser = new UserModel(req.body);
-
-   newUser.save().then(()=>{
-     // console.log('Insertion success', doc);
-     res.send('insertion success');
-   }, (err)=>{
-     // console.logeg(err);
-     res.send(err, 'not saved');
-   });
-
-   // let user = req.body;
-   // res.send('Hello '+user);
-});
-
-router.post('/updateEdu', function(req, res) {
-        UserModel.update({
-            emailId: req.body.emailId
-        }, {
-            $set: {
-                'profile.education.primary': req.body.primary,
-                'profile.education.highSchool': req.body.highSchool,
-                'profile.education.university': req.body.university
-            }
-        }, function(err) {
-            if (err) {
-                res.send('Error:' + err);
-            }
-            res.send('Updated education successfully');
-        });
-    });
-
-router.post('/updateLoc', function(req, res) {
-        UserModel.update({
-            emailId: req.body.emailId
-        }, {
-            $set: {
-                'profile.address.Line1': req.body.Line1,
-                'profile.address.Line2': req.body.Line2,
-                'profile.address.country': req.body.country,
-                'profile.address.region': req.body.region,
-                'profile.address.city': req.body.city,
-                'profile.address.postalCode': req.body.postalCode
-            }
-        }, function(err) {
-            if (err) {
-                res.send('Error:' + err);
-            }
-            res.send('Updated location successfully');
-        });
-    });
-
-router.post('/updatePro', function(req, res) {
-        UserModel.update({
-            emailId: req.body.email
-        }, {
-            $set: {
-                'profile.picture': req.body.picture,
-                'profile.description': req.body.description,
-                'profile.dob': req.body.dob,
-                'profile.gender': req.body.gender,
-                'profile.phone': req.body.phone
-            }
-        }, function(err) {
-            if (err) {
-                res.send('Error:' + err);
-            }
-            res.send('Updated userinfo successfully');
-        });
-    });
-
-router.post('/getuserprofile', function(req, res) {
-  UserModel.findOne({emailId: req.body.email}, function() {
-  // console.log('comes');
-  }).then((docs) => {
-       res.send(docs);
-   }, (err) => {
-       res.send('Cant get the docs', err);
-   });
- });
-
- router.get('/getallusers', function(req, res) {
-        UserModel.find(function(err, docs) {
-            if (err) {
-                res.send('Error:' + err);
-            } else if (docs !== null) {
-                res.send(docs);
-                // res.send('Fetched restaurant successfully')
-            } else {
-                res.send('Read Restaurant successfully');
-            }
-        });
-    });
-    router.post('/getQuestions', function(req, res) {
-        // console.log('Inside get');
-        UserModel.findOne({
-          emailId: req.body.email
-        }, function() {
-
-          //  console.log('comes');
-        }).then((docs) => {
-            res.send(docs.lists);
-        }, (err) => {
-            res.send('Cant get the docs', err);
-        });
-    });
-    router.post('/getAnswers', function(req, res) {
-         // console.log('Inside get');
-        UserModel.findOne({
-            emailId: req.body.email
-        }, function() {
-            // console.log('comes');
-        }).then((docs) => {
-            res.send(docs.answers);
-        }, (err) => {
-            res.send('Cant get the docs', err);
-        });
-    });
-    router.post('/getInterestedTopics', function(req, res) {
-      UserModel.findOne({
-          emailId: req.body.email
-      }, function() {
-          // console.log('comes');
-      }).then((docs) => {
-          res.send(docs.interestCategory);
-      }, (err) => {
-          res.send('Cant get the docs', err);
-      });
-        });
+const userDocController = require('./userdocController');
+// route for adding user
+router.post('/add', userDocController.addUser);
+// route for Update Education of user
+router.post('/updateEdu', userDocController.updateEducation);
+// route for Update Location of user
+router.post('/updateLoc', userDocController.updateLocation);
+// route for Update Profile of user
+router.post('/updatePro', userDocController.updateProfile);
+// route to get profile of user
+router.post('/getuserprofile', userDocController.getUserprofile);
+// route for getting all details of user
+router.get('/getallusers', userDocController.getallusers);
+// route for retrieving Questions posted by user
+router.post('/getQuestions', userDocController.getQuestions);
+// route for retrieving Answers posted by user
+router.post('/getAnswers', userDocController.getAnswers);
+// route for retrieving interestCategory of user
+router.post('/getInterestedTopics', userDocController.getInterestedTopics);
+// route for retrieving followers of user
+router.post('/getfollowers', userDocController.getFollowers);
+// route for retrieving followings of user
+router.post('/getfollowing', userDocController.getFollowings);
 module.exports = router;
