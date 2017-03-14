a56d2ac85e62ec15db0afea1432b39d9db0e6fd3
Soundar <soundar.ksrcts@gmail.com>
2017-03-14 11:27:52 +0000

    ip added

--- config/env/PROD.js
+++ config/env/PROD.js
@@ -1,7 +1,7 @@
-const masterMongoDBName = process.env.APP_DB || 'oxygen';
+const masterMongoDBName = process.env.APP_DB || 'zynla';
 
 const mongo = {
-	host: process.env.MONGO_HOST || '127.0.0.1',
+	host: process.env.MONGO_HOST || '192.168.1.78',
 	port: process.env.MONGO_PORT || 27017
 };
 
